cea461902a21919ffed8bd67b84d651ebfc9c7b2
sabrina <sabrinaizzahramadhani@gmail.com>
2026-03-05 20:02:32 +0700

    menambahkan input alas dan tinggi

--- week3/HitungLuasSegitiga.dart
+++ week3/HitungLuasSegitiga.dart
@@ -1,3 +1,8 @@
 void main() {
   print("Program Menghitung Luas Segitiga");
+  stdout.write("Masukkan alas: ");
+  double alas = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan tinggi: ");
+  double tinggi = double.parse(stdin.readLineSync()!);
 }
