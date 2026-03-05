f93f7739067a140e53c41c3adc98768b179be265
sabrina <sabrinaizzahramadhani@gmail.com>
2026-03-05 20:09:25 +0700

    menambahkan perulangan while agar program dapat dijalankan berulang

--- week3/HitungLuasSegitiga.dart
+++ week3/HitungLuasSegitiga.dart
@@ -1,18 +1,25 @@
 import 'dart:io';
 
 void main() {
-  print("Program Menghitung Luas Segitiga");
-  stdout.write("Masukkan alas: ");
-  double alas = double.parse(stdin.readLineSync()!);
+  String ulang = "y";
 
-  stdout.write("Masukkan tinggi: ");
-  double tinggi = double.parse(stdin.readLineSync()!);
+  while (ulang == "y") {
+    print("=== Program Menghitung Luas Segitiga ===");
+    stdout.write("Masukkan alas: ");
+    double alas = double.parse(stdin.readLineSync()!);
 
-  if (alas <= 0 || tinggi <= 0) {
-    print("Input tidak valid.");
-  } else {
-    double luas = 0.5 * alas * tinggi;
+    stdout.write("Masukkan tinggi: ");
+    double tinggi = double.parse(stdin.readLineSync()!);
 
-    print("Luas segitiga adalah: $luas");
+    if (alas <= 0 || tinggi <= 0) {
+      print("Input tidak valid.");
+    } else {
+      double luas = 0.5 * alas * tinggi;
+
+      print("Luas segitiga adalah: $luas");
+    }
+
+    stdout.write("Hitung lagi? (y/n): ");
+    ulang = stdin.readLineSync()!;
   }
 }
